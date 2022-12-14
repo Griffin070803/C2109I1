189332ce4a508878a105ec6e85d1814fc5488f8b
Griffin070803 <91366129+Griffin070803@users.noreply.github.com>
2022-12-14 13:56:03 +0700

    success

--- CSharp/QuestioMask/Program.cs
+++ CSharp/QuestioMask/Program.cs
@@ -10,6 +10,33 @@ Console.WriteLine(c + d == 0.3M ? "true" : "false");
 
 // ?: => ternary operator
 
-string fullname = null;
-//int i = null; kiểu int không được phép null, kh giá trị mặc định 0
+// ? nullable value type 
+
+string? fullname = null;
+//int i = null; kiểu int không được phép null, kh giá trị thì mặc định 0
+//c# cũ
+
 Nullable<int> i = null;//kiểu int được để null
+
+//c# mới
+
+bool? check = null;
+
+//int length = fullname != null ? fullname.Length : 0;
+
+//int? length = fullname is not null ? fullname.Length : null;
+
+//var a = 10;
+//if (a is 10)
+//{
+//}
+
+// NULL-CONDITIONAL OPERATOR " ? "
+// dấu ? ở fullname tương tự dấu !=
+//fullname mà khác null thì trả về phía sau.
+int? length = fullname?.Length;
+
+// NULL-COALESCING OPERATOR " ?? "
+// tương tự như dấu ==
+//fullname bằng null trả về vế sau
+int? chieudai = fullname?.Length ?? 0;
