1155d41f17d3a0d86a20ad9db202718acf7c826d
Griffin070803 <91366129+Griffin070803@users.noreply.github.com>
2022-12-14 15:05:31 +0700

    Ex-14-12-2022

---
+++ CSharp-14-12-2022/RedOut/UseRefOut.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefOut;
+internal class UseRefOut
+{
+    // EXPRESSION BODY
+    // khi một phương thức chỉ có một câu lệnh duy nhất
+    public void ShowInfo() =>
+        Console.WriteLine("Thông tin về class này");
+    public void ChangeNumber(ref int a, ref int b)
+    {
+        int tam = a;
+        a = b;
+        b = tam;
+        Console.WriteLine($"ChangeNumber : {nameof(a)}= {a}, {nameof(b)}= {b}");
+    }
+}
