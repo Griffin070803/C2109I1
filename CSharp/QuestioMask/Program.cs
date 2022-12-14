6df5fec0c5cceaadbaabf90f4c53963edb02a2f8
Griffin070803 <91366129+Griffin070803@users.noreply.github.com>
2022-12-14 13:54:58 +0700

    hoàn tất dấu hỏi

--- CSharp/QuestioMask/Program.cs
+++ CSharp/QuestioMask/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Formar code ctrl + k + d
+
+double a = 0.1;
+double b = 0.2;
+Console.WriteLine(a + b == 0.3 ? "true" : "false");
+
+decimal c = 0.1M;
+decimal d = 0.2M;
+Console.WriteLine(c + d == 0.3M ? "true" : "false");
+
+// ?: => ternary operator
+
+string fullname = null;
+//int i = null; kiểu int không được phép null, kh giá trị mặc định 0
+Nullable<int> i = null;//kiểu int được để null
